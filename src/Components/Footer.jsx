a9c1c6e4e6a9b3fc1c9da80eefd11f3a65cf13ad
luxian <luxianaiolos@gmail.com>
2022-06-11 20:31:18 -0700

    Found a workaround for the footer a tags
    using browser detect you can manipulate the css based on which browser is being used.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,21 +2,22 @@ import React, { useEffect } from 'react'
 import { FaGithub, FaFacebook, FaInstagram, FaMobileAlt, FaAt, FaEnvelope, FaEllipsisV, FaLinkedin } from 'react-icons/fa'
 import './Footer.css'
 import { Link } from 'react-router-dom'
-// import { Icon } from 'react-native-gradient-icon'  this did not work!!??
+import {browserName, browserVersion} from "react-device-detect"
 
 function Footer() {
+
   return (
     <div className='footer'>
       <h3 className='footer-h3'>Let's work together!</h3>
       <div className="contacts-cont">
-        <a className="contact" href='https://github.com/LucianAiolos'>
-        <svg width='0' height='0'>
-          <linearGradient id="blue-gradient" x1="00%" y1="00%" x2="0%" y2="100%" >
-            <stop stopColor='white' offset='0%'/> 
-            <stop stopColor='#38495a' offset='100%'/>
-          </linearGradient>
-        </svg>
-        <FaGithub style={{ fill: "url(#blue-gradient)", marginRight: "10px" }} />GitHub
+        <a className="contact" style={browserName = 'Safari' ? {color: 'transparent'} : {color: 'lightgray'}} href='https://github.com/LucianAiolos'>
+          <svg width='0' height='0'>
+            <linearGradient id="blue-gradient" x1="00%" y1="00%" x2="0%" y2="100%" >
+              <stop stopColor='white' offset='0%'/> 
+              <stop stopColor='#38495a' offset='100%'/>
+            </linearGradient>
+          </svg>
+          <FaGithub style={{ fill: "url(#blue-gradient)", marginRight: "10px" }} />GitHub
         </a>
         <a className="contact" href='https://www.linkedin.com/in/lucian-brooks-a6aba0232/'>
         <svg width='0' height='0'>
