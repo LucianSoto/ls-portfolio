b5bbd94e1c30ba963f78fe88797a6526311acdaf
luxian <luxianaiolos@gmail.com>
2022-06-12 19:12:01 -0700

    Finally fixed Footer contacts for safari

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,11 +6,17 @@ import {browserName, browserVersion} from "react-device-detect"
 
 function Footer() {
 
+  let browserType = browserName
+  console.log(browserName, browserVersion)
+
   return (
     <div className='footer'>
       <h3 className='footer-h3'>Let's work together!</h3>
       <div className="contacts-cont">
-        <a className="contact" style={browserName = 'Safari' ? {color: 'transparent'} : {color: 'lightgray'}} href='https://github.com/LucianAiolos'>
+        <a className="contact" 
+           style={browserType === 'Safari' ? {color: '#8a8a8a'} : {color: 'transparent'}} 
+           href='https://github.com/LucianAiolos'
+        >
           <svg width='0' height='0'>
             <linearGradient id="blue-gradient" x1="00%" y1="00%" x2="0%" y2="100%" >
               <stop stopColor='white' offset='0%'/> 
@@ -19,7 +25,9 @@ function Footer() {
           </svg>
           <FaGithub style={{ fill: "url(#blue-gradient)", marginRight: "10px" }} />GitHub
         </a>
-        <a className="contact" style={browserName = 'Safari' ? {color: 'transparent'} : {color: 'lightgray'}} href='https://www.linkedin.com/in/lucian-brooks-a6aba0232/'>
+        <a className="contact" 
+           style={browserType === 'Safari' ? {color: '#8a8a8a'} : {color: 'transparent'}} 
+           href='https://www.linkedin.com/in/lucian-brooks-a6aba0232/'>
         <svg width='0' height='0'>
           <linearGradient id="blue-gradient" x1="0%" y1="00%" x2="0%" y2="100%" >
             <stop stopColor='white' offset='0%'/> 
@@ -28,7 +36,9 @@ function Footer() {
         </svg>
         <FaLinkedin style={{ fill: "url(#blue-gradient)", marginRight: "10px"}} />LinkedIn
         </a>
-        <Link className="contact" style={browserName = 'Safari' ? {color: 'transparent'} : {color: 'lightgray'}} to='/email' >
+        <Link className="contact" 
+           style={browserType === 'Safari' ? {color: '#8a8a8a'} : {color: 'transparent'}} 
+           to='/email' >
           {/* onClick={()=> window.scroll(0,0)} */}
           <svg width='0' height='0'>
             <linearGradient id="env-gradient" x1="0%" y1="0%" x2="0%" y2="100%" >
@@ -38,7 +48,6 @@ function Footer() {
           </svg>
           <FaEnvelope style={{ fill: "url(#env-gradient)" , marginRight: "10px"}} />Email
         </Link>
-        
       </div>
     </div>
   )
