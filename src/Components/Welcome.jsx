d185759032c9b0f4a260f5edcb2a64af67e9120e
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2024-02-23 15:44:14 -0800

    changed fonts/sizing/removed blurb

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -5,7 +5,8 @@ function Welcome() {
   return (
     <div className="welcome-section">
       <div className="name-wrapper">
-        <h1>Lucian Soto</h1>
+        <h1>外ルシアン</h1>
+        {/* <h2 id='romanji'>Lucian Soto</h2> */}
       </div>
       <h3 className='welcome-h3'>Web Developer</h3>
     </div>
