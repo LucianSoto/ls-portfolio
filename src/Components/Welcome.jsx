f5044657b1802077ed41c1c9009a9dae621245a1
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2024-05-08 12:33:43 -0700

    changed name

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -5,7 +5,7 @@ function Welcome() {
   return (
     <div className="welcome-section">
       <div className="name-wrapper">
-        <h1>外ルシアン</h1>
+        <h1>Lucian Soto</h1>
         {/* <h2 id='romanji'>Lucian Soto</h2> */}
       </div>
       <h3 className='welcome-h3'>Web Developer</h3>
