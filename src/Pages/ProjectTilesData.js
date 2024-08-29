7a8497f15def6bc39506f1646aa6f0d700d13b44
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2024-08-29 13:11:25 -0700

    changed project addresses

--- src/Pages/ProjectTilesData.js
+++ src/Pages/ProjectTilesData.js
@@ -11,28 +11,28 @@ import HouseMarketplaceTwo from '../assets/images/project-images/houseMarket-til
      id: 0,
      name: "Social Media",
      image: CC,
-     address: 'https://lucianaiolos.github.io/PokeDex-Luxian/',
+     address: 'https://luciansoto.github.io/PokeDex-Luxian/',
      info: 'Thanks to the Pokemon API, I was able to build my own version of the Pokedex.'
    },
    {
      id: 1,
      name: "House Marketplace",
      image: HouseMarketplaceTwo,
-     address: 'https://lucianaiolos.github.io/House-Marketplace',
+     address: 'https://luciansoto.github.io/House-Marketplace',
      info: 'A Simple fullstack application using React and Firebase.'
    },
   {
     id: 2,
     name: "Crypto Watch",
     image: CryptoTile,
-    address: 'https://lucianaiolos.github.io/Crypto-Market/',
+    address: 'https://luciansoto.github.io/Crypto-Market/',
     info: 'A responsived website, built and deployed for client.'
   },
   // {
     //   id: 3,
   //   name: "Festival Website",
   //   image: Jungle,
-  //   address: 'https://lucianaiolos.github.io/Music-Festival-X/',
+  //   address: 'https://luciansoto.github.io/Music-Festival-X/',
   //   info: 'Two of my passions: Coding & music. This project helped bring it all together.'
   // },
   
@@ -40,7 +40,7 @@ import HouseMarketplaceTwo from '../assets/images/project-images/houseMarket-til
     id: 3,
     name: "Firebase Waitlist",
     image: Waitlist,
-    address: 'https://lucianaiolos.github.io/Waitlist-LocalStorage/',
+    address: 'https://luciansoto.github.io/Waitlist-LocalStorage/',
     projectStyle: "waitlist-style",
     info: "After using Yelp's waitlist app at one of my favorite eateries I decided to give it a shot."
   },
@@ -48,14 +48,14 @@ import HouseMarketplaceTwo from '../assets/images/project-images/houseMarket-til
     id: 4,
     name: 'Feedback App',
     image: Feedback,
-    address: 'https://lucianaiolos.github.io/Feedback-App/',
+    address: 'https://luciansoto.github.io/Feedback-App/',
     info: 'React feedback app using a JSON server for a mock back-end.'
   },
   {
     id: 5,
     name: "Pokedex",
     image: Pokedex,
-    address: 'https://lucianaiolos.github.io/PokeDex-Luxian/',
+    address: 'https://luciansoto.github.io/PokeDex-Luxian/',
     info: 'Thanks to the Pokemon API, I was able to build my own version of the Pokedex.'
   },
 ]
